588141d258e36e6a1a99166a15967748d88a1952
Sungbeom_Cho <stiger97@hanyang.ac.kr>
2023-08-29 18:45:10 +0900

    New directory name

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,8 +1,11 @@
-
-targetpath="./samples/"
+targetpath="./samples1/"
 mkdir -p $targetpath
 
-root -l -b -q ana_tthh_df.C'("tthh", "'$targetpath'")' &> ./$targetpath/log_tthh &
-root -l -b -q ana_tthh_df.C'("ttbb", "'$targetpath'")' &> ./$targetpath/log_ttbb &
-root -l -b -q ana_tthh_df.C'("ttbbbb", "'$targetpath'")' &> ./$targetpath/log_ttbbbb &
-root -l -b -q ana_tthh_df.C'("ttbbcc", "'$targetpath'")' &> ./$targetpath/log_ttbbcc &
+root -l -b -q ana_tthh_df.C'("Semi_tthh", "'$targetpath'")' &> ./$targetpath/log_tthh &
+root -l -b -q ana_tthh_df.C'("Semi_ttbb", "'$targetpath'")' &> ./$targetpath/log_ttbb &
+root -l -b -q ana_tthh_df.C'("Semi_ttbbbb", "'$targetpath'")' &> ./$targetpath/log_ttbbbb &
+root -l -b -q ana_tthh_df.C'("Semi_ttbbcc", "'$targetpath'")' &> ./$targetpath/log_ttbbcc &
+
+#targetpath="./"
+#mkdir -p $targetpath
+#root -l -b -q ana_tthh_df.C'("Test_tthh", "'$targetpath'")' &> ./$targetpath/log_tthh &
